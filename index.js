045166e2e4613dd0b35dd91e70cea9929230643e
HDahme <harrison.dahme@gmail.com>
2019-02-04 15:39:03 -0800

    Updating for sample  routes

--- index.js
+++ index.js
@@ -1,58 +1,127 @@
 // Expose this with `ngrok $PORT`
-require('dotenv').config()
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const compress = require('compression')();
-const expressValidator = require('express-validator');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const cors = require("cors");
+const request = require("request");
 
-const twilioClient = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-const { WebClient } = require('@slack/client');
+const twilioClient = require("twilio")(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+const { WebClient } = require("@slack/client");
+
 const slackClient = new WebClient(process.env.SLACK_TOKEN);
 
 const port = process.env.PORT;
 
 const corsOptions = {
-  origin: '*'
+  origin: "*"
 };
 
 const app = express();
 app.use(bodyParser());
 app.use(expressValidator());
 app.use(express.json());
-app.options('*', cors())
+app.options("*", cors());
 app.use(cors(corsOptions));
 
-app.post('/', (req, res, next) => {
+/*
+This call is the bread and butter of using Coral. It allows your to programmatically pull address trust scores
+https://docs.heycoral.com/#c502d430-c0bc-46a3-8122-a976fab8c4c5
+*/
+app.get("/trust-scores/:blockchain/:address", (req, res) => {
+  const options = {
+    url: `https://api.heycoral.com/trust?address=${
+      req.params.address
+    }&blockchain=${req.params.blockchain}`,
+    method: "GET",
+    headers: {
+      "x-api-key": process.env.YOUR_API_KEY
+    }
+  };
+
+  request(options, (err, resp, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    return res.status(200).send(body);
+  });
+});
+
+/*
+This call allows your to set up an alert on addresses you are interested in watching.
+https://docs.heycoral.com/#a4c802a5-dbd3-4f6b-b351-ae81a4e3c659
+*/
+app.post("/trust-score-alerts", (req, res) => {
+  const options = {
+    url: "https://api.heycoral.com/trust-score-alerts",
+    method: "POST",
+    headers: {
+      "x-api-key": process.env.YOUR_API_KEY
+    },
+    json: {
+      blockchain: req.body.blockchain,
+      name: req.body.name,
+      address: req.body.address,
+      url: req.body.url,
+      secret: req.body.secret,
+      action: "fraud_instance.new"
+    }
+  };
+
+  request(options, (err, resp, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    return res.status(200).send(body);
+  });
+});
+
+/*
+These routes are all third party platform integrations, to alert when a trust
+score alert is triggered
+*/
+app.post("/", (req, res) => {
   console.log(req.body);
-  return res.status(200).send(`heycoral.com 🐙 trust score alert ${req.body.name} on address ${req.body.address}; 🎉`);
+  return res
+    .status(200)
+    .send(
+      `heycoral.com 🐙 trust score alert ${req.body.name} on address ${
+        req.body.address
+      }; 🎉`
+    );
 });
 
 /*
 This integration uses Twilio to text TO_PHONE number when an alert is triggered
 */
-app.post('/twilio', (req, res, next) => {
+app.post("/twilio", (req, res) => {
   twilioClient.messages
-  .create({
-     body: `heycoral.com 🐙 trust score alert ${req.body.name} on address ${req.body.address}; 🎉`,
-     from: process.env.TWILIO_FROM_PHONE_NUMBER,
-     to: process.env.TWILIO_TO_PHONE_NUMBER
-   })
-  .then(message => console.log(message.sid))
-  .done();
+    .create({
+      body: `Coral Protocol 🚨 Name: ${req.body.name}, Address ${
+        req.body.address
+      }; 🎉`,
+      from: process.env.TWILIO_FROM_PHONE_NUMBER,
+      to: process.env.TWILIO_TO_PHONE_NUMBER
+    })
+    .then(message => console.log(message.sid))
+    .catch()
+    .done();
 
   console.log(req.body);
-  return res.status(200).send('🎉');
+  return res.status(200).send("🎉");
 });
 
 /*
 This integration uses Slack to message channel SLACK_CONVERSATION_ID when an alert is triggered
 */
-app.post('/slack', (req, res, next) => {
-  slackClient.chat.postMessage({
-    channel: process.env.SLACK_CONVERSATION_ID,
-    text: `
+app.post("/slack", (req, res) => {
+  slackClient.chat
+    .postMessage({
+      channel: process.env.SLACK_CONVERSATION_ID,
+      text: `
       🚨 Coral Protocol Trust Score Alert 🚨 \n
       🐙 *Alert Name:* \`${req.body.name}\` \n
       🐙 *Action Type:* \`${req.body.action}\` \n
@@ -60,17 +129,18 @@ app.post('/slack', (req, res, next) => {
       🐙 *Blockchain:* \`${req.body.blockchain}\` \n
       Thanks for keeping the blockchain safe 🎉
     `
-  }).then((res) => {
-    console.log('Message sent: ', res.ts);
-  })
-  .catch(console.error);
+    })
+    .then(slackRes => {
+      console.log("Message sent: ", slackRes.ts);
+    })
+    .catch(console.error);
 
   console.log(req.body);
-  return res.status(200).send('🎉');
+  return res.status(200).send("🎉");
 });
 
-app.set('port', port);
+app.set("port", port);
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Webhook Test express server listening on port ', port);
+app.listen(app.get("port"), function() {
+  console.log("Coral Protocol example app running on port ", port);
 });
